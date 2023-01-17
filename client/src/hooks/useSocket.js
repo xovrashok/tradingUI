6d000a803e7816ca10ac2201e477d4e7154c6d59
xovrasok <yuliaruda033@gmail.com>
2023-01-17 17:26:03 +0200

    feat: fix websockets

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -5,15 +5,20 @@ import config from '../config';
 const useSocket = () => {
   const [coins, setCoins] = useState('');
 
-  const { lastMessage } = useWebSocket(config.wsUri);
+  const { lastMessage } = useWebSocket(config.wsUri, {
+    shouldReconnect: (closeEvent) => true,
+    reconnectAttempts: 100,
+    reconnectInterval: 3000,
+  });
 
   useEffect(() => {
     if (lastMessage) {
       const message = JSON.parse(lastMessage?.data);
       const title = message?.actions ? message?.actions[1]?.title : '';
 
-      if (title.length > 0) {
-        setCoins(title);
+      if (title && title?.length > 0) {
+        const newTitle = title.replace(/\/.*/, '/USDT');
+        setCoins(newTitle);
       }
     }
   }, [lastMessage]);
